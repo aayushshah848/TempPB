a0185071585625d92e21b5e551a7e98888736118
SahayataCorps <agarwalsumanjio75@gmail.com>
2022-05-27 14:14:48 +0530

    updated index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const controller = require('./controller/controller');
 const express = require('express');
+const { execFile } = require("child_process");
 const app = express();
 
 app.get('/', async function (req, res) {
@@ -9,16 +10,27 @@ app.get('/', async function (req, res) {
   else {
     const file = await controller(frontEnd, backEnd).nodeController();
     res.download(file);
+    setTimeout(() => controller().cleanUp('C:\\Users\\Aayush-Yug\\Desktop\\Aerothon\\tmp\\b84d4bb0-efbb-476e-80b7-b37f19656792.zip'), 1000);
   }
 });
 
 app.get('/json', async function (req, res) {
   const frontEnd = req.query.frontend;
   const backEnd = req.query.backend;
+  const project = req.query.project;
   console.log(backEnd);
   if (frontEnd === undefined || backEnd === undefined) res.status(400);
   else {
-    const output = controller(frontEnd, backEnd).jsonController();
+      if (project == undefined) project = 'sample';
+      execFile('./trigger.sh', [frontEnd, backEnd, project],  function (error, stdout, stderr) {
+    if (error !== null) {
+      console.log(error);
+    } else {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    }
+});
+    const output = controller(project).jsonController();
     res.setHeader('content-type', 'application/json');
     res.json(output);
   }
