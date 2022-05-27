08a3bcc54568164d76e0c46f0506f3008b0732d5
SahayataCorps <agarwalsumanjio75@gmail.com>
2022-05-27 16:19:34 +0530

    updated trigger

--- trigger.sh
+++ trigger.sh
@@ -1,5 +1,12 @@
 if [ $1 = "react" ] && [ $2 = "django" ]
 then
-django-admin startproject ./basePackages/$3 --template basePackages/django-react-template 
+django-admin startproject $3 --template basePackages/django-react-template
+mv $3 basePackages/$3
 # cp -r ./basePackages/django-react-template ./basePackages/$3
+fi
+
+if [ $1 = "angular" ] && [ $2 = "django" ]
+then
+django-admin startproject $3 --template basePackages/django-ng-boilerplate
+mv $3 basePackages/$3
 fi
